7b9ed28a2a07f73722f207d83610038ab1de79d8
Becca Nelson <beccanelson88@gmail.com>
2016-07-26 12:01:42 -0500

    Update board view based on game type
    This commit adds a GameConfig class which stores the game type
    depending on which game was selected in the home view. The text
    on the board view updates based on the game type.

---
+++ iOS-tic-tac-toeTests/ViewControllerTests.swift
@@ -0,0 +1,40 @@
+@testable import iOS_tic_tac_toe
+import Quick
+import Nimble
+
+class ViewControllerSpec: QuickSpec {
+    override func spec() {
+        let controller = ViewController()
+        let button = UIButton()
+    
+        describe("View Controller") {
+            
+            it("can set the game type to HumanVsHuman") {
+                controller.newPlayerVsPlayerGame(button)
+                expect(GameConfig.gameType).to(equal(GameConfig.humanVsHuman))
+            }
+            
+            it("can set the game type to HumanVsComputer") {
+                let clickedButton = self.buttonForGameType(button, gameType: GameConfig.humanVsComputer)
+                controller.newPlayerVsComputerGame(clickedButton)
+                expect(GameConfig.gameType).to(equal(GameConfig.humanVsComputer))
+            }
+            
+            it("can set the game type to ComputerVsHuman") {
+                let clickedButton = self.buttonForGameType(button, gameType: GameConfig.humanVsComputer)
+                button.tag = 1
+                controller.newPlayerVsComputerGame(clickedButton)
+                expect(GameConfig.gameType).to(equal(GameConfig.computerVsHuman))
+            }
+        }
+    }
+    
+    func buttonForGameType(button: UIButton, gameType: String) -> UIButton {
+        if (gameType == GameConfig.computerVsHuman) {
+            button.tag = 1
+        } else {
+            button.tag = 0
+        }
+        return button
+    }
+}
