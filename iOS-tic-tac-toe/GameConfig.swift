1d2327bc4ea208960f6c7d6c1f0eda711d118aea
Becca Nelson <beccanelson88@gmail.com>
2016-08-04 10:00:44 -0500

    Remove settings button from home screen

--- iOS-tic-tac-toe/GameConfig.swift
+++ iOS-tic-tac-toe/GameConfig.swift
@@ -1,6 +1,6 @@
 import Foundation
 public class GameConfig {
-    public static let serverURL = "http://localhost:5000/game"
+    public static let serverURL = "http://stormy-savannah-24890.herokuapp.com/game"
     
     public static let humanVsHuman = "humanVsHuman"
     public static let humanVsComputer = "humanVsComputer"
